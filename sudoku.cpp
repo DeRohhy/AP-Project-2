9184bda79ebf249f2ab0ca034381691f4585c7f3
RohhyDohhy <rohhy.business@gmail.com>
2026-05-09 17:31:50 +0330

    Add output implementation and solve() function foundation

--- sudoku.cpp
+++ sudoku.cpp
@@ -11,6 +11,12 @@
 
 const int SIZE = 9;
 
+enum class SolveResult
+{
+    SOLVABLE,
+    UNSOLVABLE
+};
+
 void readInput(int board[][SIZE])
 {
     for (int i = 0; i < SIZE; i++)
@@ -18,11 +24,32 @@ void readInput(int board[][SIZE])
             std::cin >> board[i][j];
 }
 
+SolveResult solve(int board[][SIZE])
+{
+
+
+    return SolveResult::UNSOLVABLE;
+}
+
+void printBoard(int board[][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            std::cout << board[i][j] << " \n"[j == SIZE - 1];
+}
+
 int main()
 {
     int board[SIZE][SIZE];
 
     readInput(board);
+
+    SolveResult result = solve(board);
+
+    if (result == SolveResult::SOLVABLE)
+        printBoard(board);
+    else
+        std::cout << "No solution exists" << std::endl;
 
     return 0;
 }
