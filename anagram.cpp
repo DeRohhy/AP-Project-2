dfb118362ff402e0a32c2f04cea02a4ed5e14a4d
RohhyDohhy <rohhy.business@gmail.com>
2026-05-13 21:27:28 +0330

    Add Trie data structure and readDictionary function

--- anagram.cpp
+++ anagram.cpp
@@ -1,6 +1,6 @@
 /*
 * ========== Naming Convention Guideline ==========
-* Classes & Enums: PascalCase
+* Classes, Structs & Enums: PascalCase
 * Functions: camelCase
 * Variables: lower_snake_case
 * Constants: UPPER_SNAKE_CASE
@@ -8,9 +8,75 @@
 */
 
 #include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+
+
+class Trie
+{
+public:
+    Trie() { root = new Node; }
+
+    ~Trie() { delete root; }
+
+    void insert(const std::string& word);
+
+private:
+    struct Node
+    {
+        std::map<char, Node*> children;
+        bool is_end_of_word = false;
+
+        ~Node()
+        {
+            for (auto& [key, node]: children)
+                delete node;
+        }
+    };
+
+    Node* root;
+};
+
+void Trie::insert(const std::string& word)
+{
+    Node* current = root;
+
+    for (char letter: word)
+    {
+        if (current->children.find(letter) == current->children.end())
+            current->children[letter] = new Node;
+
+        current = current->children[letter];
+    }
+
+    current->is_end_of_word = true;
+}
+
+void readDictionary(Trie& dictionary)
+{
+    int n;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::string word;
+        std::cin >> word;
+
+        dictionary.insert(word);
+    }
+}
+
+void runGame()
+{
+    Trie dictionary;
+
+    readDictionary(dictionary);
+}
 
 int main()
 {
-
+    runGame();
     return 0;
 }
